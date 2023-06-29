951d48e3a8ce7e75a335d5a071dcd0e053c9c672
Vikash <kvikash09@outlook.com>
2023-06-29 17:10:40 +0530

    comparison operator

--- Javascript/app.js
+++ Javascript/app.js
@@ -59,14 +59,35 @@ document.getElementById("detail").innerHTML = Employee.name;
 // console.log(result);
 
 // Assignment Operator
-let coins = 10;
-let coinsPerEnemy = 5;
-document.write("Result will be:");
-document.write(
-  coins++ -
-    ++coinsPerEnemy +
-    ++coins -
-    ++coinsPerEnemy +
-    coins-- -
-    (((--coinsPerEnemy * ++coins) / ++coinsPerEnemy) % coins--)
-);
+// let coins = 10;
+// let coinsPerEnemy = 5;
+// document.write("Result will be:");
+// document.write(
+//   coins++ -
+//     ++coinsPerEnemy +
+//     ++coins -
+//     ++coinsPerEnemy +
+//     coins-- -
+//     (((--coinsPerEnemy * ++coins) / ++coinsPerEnemy) % coins--)
+// );
+// Comparison Operator
+let team1Score = 350;
+let team2Score = 260;
+let teamWin1 = team1Score > team2Score;
+document.write("Has Team 1 won: " + teamWin1);
+document.write("<br>");
+let team3Score = 280;
+let team4Score = 275;
+let teamWin2 = team3Score < team4Score;
+document.write("Has Team 3 won: " + teamWin2);
+document.write("<br>");
+let team5Score = 320;
+let team6Score = 320;
+let teamWin3 = team5Score >= team6Score;
+document.write("Has Team 4 won: " + teamWin3);
+document.write("<br>");
+let team7Score = 270;
+let team8Score = 260;
+let teamWin4 = team7Score <= team8Score;
+document.write("Has Team 5 won: " + teamWin4);
+document.write("<br>");
